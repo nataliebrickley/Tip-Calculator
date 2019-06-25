81da667a133e74b33ecb3aa3ed967548994a1439
Natalie <natt_rose@yahoo.com>
2019-06-25 10:37:34 -0700

    added form elements

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,28 @@ class App extends Component {
       <div className="App">
         <div className="calc">
           <h1>Tip Calculator</h1>
+          <form>
+            <div class="form-group">
+              <label for="bill">How much was your bill?</label><br />
+              $ <input type="text" class="form-control" id="bill" />
+            </div>
+            <div class="form-group">
+              <label for="tip-percent">How was your service?</label><br />
+              <select class="form-control" id="tip-percent">
+                <option disabled selected value> -- select an option -- </option>
+                <option>30% Outstanding</option>
+                <option>20% Good</option>
+                <option>15% Okay</option>
+                <option>10% Mediocre</option>
+                <option>5% Terrible</option>
+              </select>
+            </div>
+            <div class="form-group">
+              <label for="number of people">How many people are sharing the bill?</label><br />
+              <input type="text" class="form-control" id="people" />
+            </div>
+            <button>CALCULATE</button>
+          </form>
         </div>
       </div>
     );
